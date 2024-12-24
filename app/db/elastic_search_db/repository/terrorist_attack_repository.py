3deda9aba497d724ebf6d51be81f65ecba8d996f
Ariel <relbh18@gmail.com>
2024-12-24 16:25:22 +0200

    add function to create indexes to mongo db

--- app/db/elastic_search_db/repository/terrorist_attack_repository.py
+++ app/db/elastic_search_db/repository/terrorist_attack_repository.py
@@ -1,5 +1,7 @@
 from elasticsearch.helpers import bulk
 from app.db.elastic_search_db.database import terrorist_attacks_history_index, elastic_client
+from app.db.mongo_db.database import terrorist_attacks_collection
+from pymongo import ASCENDING
 
 
 
@@ -21,6 +23,17 @@ def insert_attacks_to_elastic_db(terrorist_attacks: list):
         return {'Success': f'Inserted {success} records'}
     except Exception as e:
         return {'Error': f'Exception occurred: {str(e)}'}
+
+
+def create_indexes_to_mongo():
+    terrorist_attacks_collection.create_index([('casualties.deadly_grade', 1)])
+    terrorist_attacks_collection.create_index([('location.full_date', 1)])
+    terrorist_attacks_collection.create_index([('attack.attack_type', 1)])
+    terrorist_attacks_collection.create_index([('attack.target', 1)])
+    terrorist_attacks_collection.create_index([('location.region', 1)])
+    terrorist_attacks_collection.create_index([('location.country', 1)])
+
+    print('indexes created successfully')
 
 
 def delete_all_docs_all_indexes():
--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
+from app.db.elastic_search_db.repository.terrorist_attack_repository import create_indexes_to_mongo
 from app.service.read_files_service import insert_data_to_db
 
 
 if __name__ == '__main__':
     insert_data_to_db()
+    create_indexes_to_mongo()
+
 
 
 
